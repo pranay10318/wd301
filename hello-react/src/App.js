1430b4fe75e6df3eddff3b4b30a6caed6d8672e4
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-04-13 15:50:58 +0530

    some css applied

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
         Smater Tasks
       </h1>
       <p className="text text-center"><b>Project:</b>Graduation Final Year Project(Revamp College Website)</p>
-      <div className="float-left border border-grey p-2 ml-60">
+      <div className="float-left border border-grey p-4 ml-60">
         <h1 className="bg-slate-100 text-2xl">Pending</h1>
         <TaskCard
           title="Build the website with static content"
@@ -19,10 +19,10 @@ function App() {
           dueDate="6-04-2023"
           assigneeName="Rohit M"
         />
-        <button className="bg-gray-300 rounded p-2">+NewTask</button>
+        <button className="bg-gray-300 rounded p-1 mt-2">+NewTask</button>
       </div>
 
-      <div className="float-right mr-60">
+      <div className="float-right border border-grey p-4 mr-60">
         <h1 className="bg-slate-100 text-2xl">Done</h1>
         <TaskCard
           title="Design the mockup"
--- hello-react/src/TaskCard.js
+++ hello-react/src/TaskCard.js
@@ -3,6 +3,7 @@ const TaskCard = (props) => {
   if (props.dueDate) {
     return (
       <div className="TaskItem">
+        <br/>
         <h2 className="text-xl font-bold">{props.title}</h2>
         <p>Due on: {props.dueDate}</p>
         <p>Assignee: {props.assigneeName}</p>
@@ -11,6 +12,7 @@ const TaskCard = (props) => {
   }
   return (
     <div className="TaskItem">
+        <br/>
       <h2 className="text-xl font-bold">{props.title}</h2>
       <p>Completed on: {props.completedAtDate}</p>
       <p>Assignee: {props.assigneeName}</p>
