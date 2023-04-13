dabf3a2b163b17aeb55d6d6fd523930d55e971d8
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-04-13 23:22:51 +0530

    full fledged css added

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,15 +4,15 @@ function App() {
   return (
     <div class="grid grid-cols-6 m-10">
       <div class="col-start-2 col-span-5">
-        <h1 className="text-3xl font-extrabold">
+        <p className="text-3xl font-extrabold">
           Smater Tasks
-        </h1>
+        </p>
         <p className="text-xl"><b className="mr-1">Project:</b>Graduation Final Year Project(Revamp College Website)</p>
       </div>
       
       <div class="mt-4 col-start-2 col-span-4 space-x-10">
         <div className="float-left border border-black p-3 rounded-2xl pb-24">
-          <h1 className="text-xl text-center mb-2 font-semibold">Pending</h1>
+          <p className="text-xl text-center mb-2 font-semibold">Pending</p>
           <TaskCard
             title="Build the website with static content"
             dueDate="10th April"
@@ -27,7 +27,7 @@ function App() {
         </div>
 
         <div className="border border-black rounded-2xl p-4 pb-32 float-left">
-          <h1 className="text-xl font-semibold text-center mb-2">Done</h1>
+          <p className="text-xl font-semibold text-center mb-2">Done</p>
           <TaskCard
             title="Design the mockup"
             completedAtDate="10th April"
