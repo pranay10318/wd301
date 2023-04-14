a1ec26d70d6a6dafedf227c2574be97a16b413b5
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-04-14 12:23:16 +0530

    13th submission

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,62 +1,37 @@
-import "./App.css";
 import TaskCard from "./TaskCard";
+
+
 function App() {
+
   return (
     <div>
-      <div className="grid grid-cols-6 m-3">
-
-        <div class="col-start-2 col-span-4">
-          <center>
+      <div class="grid grid-cols-6">
+          <div class="col-start-2 col-span-4">
             <p class="text-3xl font-bold">Smarter Tasks</p>
-            <p class="text-1xl mb-4">Project: Graduation Final Year Project (Revamp College Website)</p>      
-          </center>
+            <p class="text-xl mb-4">Project: Graduation Final Year Project (Revamp College Website)</p>      
+          </div>
+          <div class=" col-start-2 col-span-2 border border-black rounded-2xl mr-3">
+              <p class="py-2 text-2xl px-5 text-center font-bold"> Pending </p>
+              <div class="border border-gray-900 m-3 py-1 px-5">
+                <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Rohith S"/>
+              </div>
+              <div class="border border-gray-900 m-3 py-2 px-5">
+                <TaskCard title="Add a blog" dueDate="22nd March" assigneeName="Rohith M"/>
+              </div>
+              <div class="border border-gray-900 m-3 py-2 px-5 bg-gray-300 ">
+                <p>+New Task</p>
+              </div>
+          </div>
+          <div class=" col-start-4 col-span-2 border border-gray-900 rounded-xl ml-3">
+              <p class="text-2xl text-center font-bold py-2 px-5"> Done </p>
+              <div class=" border border-gray-900 m-3 py-2 px-5">
+                <TaskCard title="Design the mockup" completedAtDate="10th April" assigneeName="Rohith M"/>
+              </div>
+              <div class=" border border-gray-900 m-3 py-2 px-5">
+                <TaskCard title="Get the approval from principal" completedAtDate="20th April" assigneeName="Ajay S"/>
+              </div>
+          </div>             
         </div>
-
-        {/* left side column */}
-        <div className="col-start-2 col-span-2 border border-black rounded-2xl p-2 pb-10 mr-3 ">
-          <p className="text-2xl text-center font-bold mb-2">Pending</p>
-          <div className="border border-black px-2 py-5 m-3 rounded">
-            <TaskCard
-              title="Build a website with static content"
-              dueDate="10th April"
-              assigneeName="Rohit S"
-            />
-          </div>
-          <div className="border border-black px-2 py-5 m-3 rounded">
-            <TaskCard
-              title="Add a blog"
-              dueDate="22nd March"
-              assigneeName="Rohit M"
-            />
-          </div>
-          <div className="border border-gray  p-1 m-3 rounded">
-            <button className="bg-gray-400 w-full text-left rounded p-1">
-              + New task
-            </button>
-          </div>
-        </div>
-
-        {/* right side column */}
-        <div className="col-start-4 col-span-2 border border-black rounded-2xl p-2 ml-3">
-          <p className="text-2xl text-center font-bold mb-2">Done</p>
-          <div className="border border-black px-2 py-5 m-3 rounded">
-            <TaskCard
-              title="Design the Mockup"
-              completedAtDate="10th April"
-              assigneeName="Rohit M"
-            />
-          </div>
-          <div className="border border-black px-2 py-5 m-3 rounded">
-            <TaskCard
-              title="Get the approval from principal"
-              completedAtDate="20th April"
-              assigneeName="Ajay S"
-            />
-          </div>
-        </div>
-
-      </div>
-      {/* grid close */}
     </div>
   );
 }
--- hello-react/src/TaskCard.js
+++ hello-react/src/TaskCard.js
@@ -1,22 +1,13 @@
 import './TaskCard.css'
-
 const TaskCard = (props) => {
-  if(props.dueDate) {
+    let x = "Due on: " + props.dueDate
+    if(!props.dueDate) x = "Completed on: " + props.completedAtDate 
     return (
-      <div className='TaskItem'>
-        <p className="text-xl font-bold mb-2">{props.title}</p>
-        <p>Due on: {props.dueDate}</p>
-        <p>Assignee: {props.assigneeName}</p>
-      </div>
+        <div className='TaskItem'>
+            <p className='text-m font-bold'> {props.title} </p>
+            <p>{x}</p>
+            <p> Assignee: {props.assigneeName}  </p>
+        </div>
     );
-  }
-  return (
-    <div className='TaskItem'>
-      <p className="text-xl font-bold mb-2">{props.title}</p>
-      <p>Completed on: {props.completedAtDate}</p>
-      <p>Assignee: {props.assigneeName}</p>
-    </div>
-  )
 }
-
 export default TaskCard
