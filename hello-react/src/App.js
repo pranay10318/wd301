e0f7acade0066a446b31617ab8f997d2cd216572
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-04-14 09:53:29 +0530

    refined app

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,46 +2,32 @@ import "./App.css";
 import TaskCard from "./TaskCard";
 function App() {
   return (
-    <div class="grid grid-cols-6 m-10">
-      <div class="col-start-2 col-span-5">
-        <p className="text-3xl font-extrabold">
+    <div className = "grid grid-cols-6 m-10">
+      <div className = "col-start-2 col-span-5 m-3">
+        <p className="text-3xl font-bold">
           Smater Tasks
         </p>
         <p className="text-xl"><b className="mr-1">Project:</b>Graduation Final Year Project(Revamp College Website)</p>
       </div>
-      
-      <div class="mt-4 col-start-2 col-span-4 space-x-10">
-        <div className="float-left border border-black p-3 rounded-2xl pb-24">
-          <p className="text-xl text-center mb-2 font-semibold">Pending</p>
-          <TaskCard
-            title="Build the website with static content"
-            dueDate="10th April"
-            assigneeName="Rohit S"
-          />
-          <TaskCard
-            title="Add a blog"
-            dueDate="22nd March"
-            assigneeName="Rohit M"
-          />
-          <button className="bg-gray-300 text-left font-semibold rounded p-1 mt-2 w-full">+ New task</button>
-        </div>
 
-        <div className="border border-black rounded-2xl p-4 pb-32 float-left">
-          <p className="text-xl font-semibold text-center mb-2">Done</p>
-          <TaskCard
-            title="Design the mockup"
-            completedAtDate="10th April"
-            assigneeName="Rohit M"
-          />
-          <TaskCard
-            title="Get the approval from principal"
-            completedAtDate="20th April"
-            assigneeName="Ajay S"
-          />
-        </div>
+      <div className = "col-start-2 col-span-4 space-x-5">
+          <div className = "float-left border border-black rounded-2xl p-4">
+            <p className = "text-2xl text-center font-bold mb-2">Pending</p>
+            <TaskCard title = "Build a website with static content" dueDate = "10th April" assigneeName = "Rohit S"/>
+            <TaskCard title = "Add a blog" dueDate = "22nd March" assigneeName = "Rohit M"/>
+            <button className = "bg-gray-400 w-full text-left rounded p-1">+ New task</button>
+          </div>
+
+          <div className = "float-left border border-black rounded-2xl p-4 ">
+            <p className = "text-2xl text-center font-bold mb-2">Done</p>
+            <TaskCard title = "Design the Mockup" completedAtDate = "10th April" assigneeName = "Rohit M"/>
+            <TaskCard title = "Get the approval from principal" completedAtDate = "20th April" assigneeName = "Ajay S"/>
+          </div>
       </div>
 
     </div>
   );
 }
+
+
 export default App;
--- hello-react/src/TaskCard.js
+++ hello-react/src/TaskCard.js
@@ -1,21 +1,23 @@
-import "./TaskCard.css";
+import './TaskCard.css'
+
 const TaskCard = (props) => {
-  if (props.dueDate) {
+  console.log(props)
+  if(props.dueDate) {
     return (
-      <div className="TaskItem border border-black rounded mt-2 p-1">
-        <h2 className="text-xl font-bold">{props.title}</h2>
+      <div className='TaskItem'>
+        <h2 className="text-xl font-bold mb-2">{props.title}</h2>
         <p>Due on: {props.dueDate}</p>
         <p>Assignee: {props.assigneeName}</p>
       </div>
     );
   }
   return (
-    <div className="TaskItem border border-black rounded mt-2 p-1">
-      <h2 className="text-xl font-bold">{props.title}</h2>
+    <div className='TaskItem'>
+      <h2 className="text-xl font-bold mb-2">{props.title}</h2>
       <p>Completed on: {props.completedAtDate}</p>
       <p>Assignee: {props.assigneeName}</p>
     </div>
-  );
-};
+  )
+}
 
-export default TaskCard;
+export default TaskCard
