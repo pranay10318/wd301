1b92a592f0c780190e1b0023e2adfd57a574aae2
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-08-07 17:57:07 +0530

    merge coflict

--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -9,7 +9,7 @@ import Signup from "../pages/signup";
 import Projects from "../pages/projects";
 import Members from "../pages/members";
 import Logout from "../pages/logout";
-import NotFound from "../Notfound";
+import NotFound from "../pages/Notfound";
 
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/account/projects" replace /> },
