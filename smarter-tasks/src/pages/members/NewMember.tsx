350963e1640a28eaab2e943ecd82e50b0e0a8508
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-08-07 18:27:43 +0530

    last commit lvl-7

--- smarter-tasks/src/pages/members/NewMember.tsx
+++ smarter-tasks/src/pages/members/NewMember.tsx
@@ -128,6 +128,7 @@ const NewMember = () => {
                         Submit
                       </button>
                       <button
+                        type="submit"
                         onClick={closeModal}
                         className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       >
