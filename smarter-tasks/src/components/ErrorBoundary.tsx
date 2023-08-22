b46d7551e98862784a7483ad8a2909303b2c6b7f
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-08-22 21:21:57 +0530

    build err

--- smarter-tasks/src/components/ErrorBoundary.tsx
+++ smarter-tasks/src/components/ErrorBoundary.tsx
@@ -1,11 +1,15 @@
-import React, { Component, ErrorInfo, ReactNode } from "react";
+import React, { Component, ErrorInfo, ReactNode, ReactElement } from "react";
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
 
 interface ErrorBoundaryState {
   hasError: boolean;
 }
 
-class ErrorBoundary extends Component<{}, ErrorBoundaryState> {
-  constructor(props: {}) {
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
     super(props);
     this.state = { hasError: false };
   }
