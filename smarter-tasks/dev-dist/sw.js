9e1845159071a3d85cdc736bc02a7eaef79eaf9c
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-08-23 21:11:27 +0530

    l-9

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -83,11 +83,11 @@ define(["./workbox-8cc11778"], function (workbox) {
     [
       {
         url: "registerSW.js",
-        revision: "3ca0b8505b4bec776b69afdba2768812",
+        revision: "2c81d9b0ad45b1d70d93e4e5be816fdc",
       },
       {
         url: "index.html",
-        revision: "0.fqmto310pk",
+        revision: "0.6fvh2tmfang",
       },
     ],
     {}
